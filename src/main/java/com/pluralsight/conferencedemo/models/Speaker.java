6a264d1a5133e050ed276109535cd61ab97414d9
RENATU BONFIM <renatu_mb@yahoo.com.br>
2020-12-06 16:39:06 -0800

    Treating SERIALIZATION

--- src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
@@ -1,11 +1,14 @@
 package com.pluralsight.conferencedemo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Speaker {
 
     @Id
@@ -20,7 +23,9 @@ public class Speaker {
     @Lob
     @Type(type = "org.hibernate.type.BinaryType")
     private byte[] speaker_photo;
+
     @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
     private List<Session> sessions;
 
     public byte[] getSpeaker_photo() {
